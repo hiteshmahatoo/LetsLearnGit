dcf9419744ec065d75e14b7e5f06a3017331d0ff
Hitesh Mahatoo <hiteshmahatoo4@gmail.com>
2022-08-24 10:02:39 +0530

    Initial Commit

---
+++ sum1.java
@@ -0,0 +1,9 @@
+public class sum1 {
+        public static void main(String args[]) {
+            int a = 5;
+            int b = 4;
+            int sum = a+b;
+            System.out.println(sum);
+        }
+    }
+
